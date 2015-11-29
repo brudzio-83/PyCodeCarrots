c222006bb7f6040e1737f1c50d15fec0aa550955
Agnieszka <brudzio-83@o2.pl>
2015-11-29 19:18:46 +0100

    zmiana

--- klasa3.py
+++ klasa3.py
@@ -8,22 +8,13 @@ class człowiek:
 		self.wiek = self.wiek + 10
 
 
-ala = człowiek ()
-ala.waga = 50
-ala.wiek = 20
-print (ala.waga)
-print (ala.wiek)
-
 aga = człowiek ()
 aga.waga = 45
 aga.wiek = 30
+
 print (aga.waga)
 print (aga.wiek)
 
-ala.jesc ()
-print (ala.waga)
-
 aga.jesc ()
 print (aga.waga)
-
-
+print (aga.waga)
